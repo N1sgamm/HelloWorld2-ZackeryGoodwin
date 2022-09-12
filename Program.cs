da5fa677d51869c73023218fb546a0d5411fab8b
N1sgamm <N1sgamm@gmail.com>
2022-09-12 11:38:04 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld2_ZackeryGoodwin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 2");
+            Console.WriteLine("~~~~~~~~~~~~~");
+            Console.WriteLine(""); // This is a blank line
+            Console.WriteLine("Zackery Goodwin");
+            Console.WriteLine("09/12/2022");
+            Console.WriteLine("");
+            Console.WriteLine("Press any button to exit");
+            Console.ReadKey(true);
+            Console.Beep(1000, 100);
+        }
+    }
+}
